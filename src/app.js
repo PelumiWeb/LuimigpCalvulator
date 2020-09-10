8861f1f814b5aa838db16a02f42cb1fde87dfae5
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-09-09 22:29:10 -0700

    using a view engine

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3030;
-const publicDirectlyPath = path.join(__dirname, "../public");
+app.set("view engine", "ejs");
+// const publicDirectlyPath = path.join(__dirname, "../public");
+app.use(express.static("public"));
 
-app.use(express.static(publicDirectlyPath));
+// app.use(express.static(publicDirectlyPath));
 
 app.listen(port, () => {
   console.log(`app is running in port ${port}`);
