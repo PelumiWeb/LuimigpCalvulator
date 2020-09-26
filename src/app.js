4257b53e9ced578ab48723bd6a0ca8c004a46f8b
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-09-25 17:21:21 -0700

    making sure it works

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ const port = process.env.PORT || 3030;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.render("index", {
-    name: "Pelumi",
-  });
+  res.render("index");
 });
 
 app.listen(port, () => {
